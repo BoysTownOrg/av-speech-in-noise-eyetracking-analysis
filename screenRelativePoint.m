6e7990e1da3923b8bb83dfa0185e831fbf76a3d9
Seth Bashford <seth.bashford@protonmail.com>
2021-08-11 13:40:26 -0500

    refactor

--- screenRelativePoint.m
+++ screenRelativePoint.m
@@ -1,4 +1,8 @@
 function point = screenRelativePoint(screen, video, videoRelativePoint)
-point.x = video.pixels.width * video.scaling * (videoRelativePoint.x - 0.5)/screen.pixels.width + 0.5;
-point.y = video.pixels.height * video.scaling * (videoRelativePoint.y - 0.5)/screen.pixels.height + 0.5;
+point.x = screenRelativeCoordinate(videoRelativePoint.x, screen.pixels.width, video.pixels.width, video.scaling);
+point.y = screenRelativeCoordinate(videoRelativePoint.y, screen.pixels.height, video.pixels.height, video.scaling);
+end
+
+function coordinate = screenRelativeCoordinate(videoRelativeCoordinate, screenPixels, videoPixels, videoScaling)
+coordinate = videoPixels * videoScaling * (videoRelativeCoordinate - 0.5)/screenPixels + 0.5;
 end
