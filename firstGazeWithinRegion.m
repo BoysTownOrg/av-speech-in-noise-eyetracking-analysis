72bfd743ff54823bbf93cd709dcda137ea5f984d
Seth Bashford <seth.bashford@protonmail.com>
2021-08-11 15:27:20 -0500

    refactor

--- firstGazeWithinRegion.m
+++ firstGazeWithinRegion.m
@@ -1,15 +1,9 @@
 function index = firstGazeWithinRegion(points, region)
 index = 1;
 while index ~= length(points) + 1
-    if ...
-            within(points(index).x, region.x, region.width) && ...
-            within(points(index).y, region.y, region.height)
+    if regionContains(region, points(index))
         return
     end
     index = index + 1;
 end
-end
-
-function answer = within(coordinate, lowerEnd, extent)
-answer = coordinate >= lowerEnd && coordinate <= lowerEnd + extent;
 end
