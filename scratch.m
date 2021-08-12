8f91f85972e9c91b645a4d6dee90d6a77860a86a
Seth Bashford <seth.bashford@protonmail.com>
2021-08-12 13:57:03 -0500

    better

--- scratch.m
+++ scratch.m
@@ -1,35 +1,45 @@
-test = parseAvSpeechEyetrackingOutput("Subject_test_Session__Experimenter_test_2021-6-26-16-15-52.txt");
+function fixations = scratch
+filename = "Subject_test_Session__Experimenter_test_2021-6-26-16-15-52.txt";
 screen.pixels.height = 1080;
 screen.pixels.width = 1920;
 video.pixels.height = 1080;
 video.pixels.width = 1920;
 video.scaling = 2/3;
 fixation.threshold.us = 90000;
-roi.x = 0.508;
-roi.y = 0.518;
-roi.width = 0.03;
-roi.height = 0.03;
+trial = 6;
+roi.x = 0.375;
+roi.y = 0.375;
+roi.width = 0.25;
+roi.height = 0.25;
+
+test = parseAvSpeechEyetrackingOutput(filename);
 fixations = [];
 lastGazeIndexOutsideROI = 0;
-while 1
-    firstGazeIndexWithinROI = lastGazeIndexOutsideROI + firstGazeWithinRegion([test.eyetracking(1).gaze(lastGazeIndexOutsideROI + 1:end-1).left], screenRelativeRegion(screen, video, roi));
-    if firstGazeIndexWithinROI == length(test.eyetracking(1).gaze)
-        break
-    else
-        lastGazeIndexOutsideROI = firstGazeIndexWithinROI + 1;
-        while regionContains(roi, videoRelativePoint(screen, video, test.eyetracking(1).gaze(lastGazeIndexOutsideROI).left))
-            lastGazeIndexOutsideROI = lastGazeIndexOutsideROI + 1;
-            if lastGazeIndexOutsideROI == length(test.eyetracking(1).gaze) + 1
-                break
-            end
-        end
-        lastGazeIndexWithinROI = lastGazeIndexOutsideROI - 1;
-        if lastGazeIndexWithinROI ~= firstGazeIndexWithinROI && gazeDuration_us(test.eyetracking(1).gaze(firstGazeIndexWithinROI:lastGazeIndexWithinROI)) >= fixation.threshold.us
-            fixations(end+1).duration_ms = gazeDuration_ms(test.eyetracking(1).gaze(firstGazeIndexWithinROI:lastGazeIndexWithinROI));
-            fixations(end).targetStartRelativeTime_ms = targetStartRelativeTime_ms(test.eyetracking(1), test.eyetracking(1).gaze(firstGazeIndexWithinROI).time_us);
-        end
-        if lastGazeIndexOutsideROI > length(test.eyetracking(1).gaze) - 2
-            break
-        end
+while lastGazeIndexOutsideROI < gazeSamples(test, trial) - 1
+    firstGazeIndexWithinROI = lastGazeIndexOutsideROI + ...
+        firstGazeWithinRegion(...
+            [gazeBetween(test, trial, lastGazeIndexOutsideROI+1, gazeSamples(test, trial)-1).left], ...
+            screenRelativeRegion(screen, video, roi));
+    lastGazeIndexOutsideROI = firstGazeIndexWithinROI + 1;
+    while lastGazeIndexOutsideROI ~= gazeSamples(test, trial) + 1 && regionContains(roi, ...
+            videoRelativePoint(screen, video, test.eyetracking(trial).gaze(lastGazeIndexOutsideROI).left))
+        lastGazeIndexOutsideROI = lastGazeIndexOutsideROI + 1;
     end
+    lastGazeIndexWithinROI = lastGazeIndexOutsideROI - 1;
+    if lastGazeIndexWithinROI ~= firstGazeIndexWithinROI && ...
+            gazeDuration_us(gazeBetween(test, trial, firstGazeIndexWithinROI, lastGazeIndexWithinROI)) >= fixation.threshold.us
+        fixations(end+1).duration_ms = gazeDuration_ms(...
+            gazeBetween(test, trial, firstGazeIndexWithinROI, lastGazeIndexWithinROI));
+        fixations(end).targetStartRelativeTime_ms = targetStartRelativeTime_ms(...
+            test.eyetracking(trial), test.eyetracking(trial).gaze(firstGazeIndexWithinROI).time_us);
+    end
+end
+end
+
+function samples = gazeSamples(test, trial)
+samples = length(test.eyetracking(trial).gaze);
+end
+
+function gaze = gazeBetween(test, trial, first, last)
+gaze = test.eyetracking(trial).gaze(first:last);
 end
