b04e6d5f0855b1c68e3ddbf912b9207c041ecfd7
Seth Bashford <seth.bashford@protonmail.com>
2021-08-12 11:17:45 -0500

    betterish

--- scratch.m
+++ scratch.m
@@ -4,34 +4,33 @@ screen.pixels.width = 1920;
 video.pixels.height = 1080;
 video.pixels.width = 1920;
 video.scaling = 2/3;
+fixation.threshold.us = 90000;
 roi.x = 0.508;
 roi.y = 0.518;
 roi.width = 0.03;
 roi.height = 0.03;
 fixations = [];
-head = 1;
+firstGazeIndexWithinROI = 0;
 while 1
-    head = head - 1 + firstGazeWithinRegion([test.eyetracking(1).gaze(head:end-1).left], screenRelativeRegion(screen, video, roi));
-    if head == length(test.eyetracking(1).gaze)
+    firstGazeIndexWithinROI = firstGazeIndexWithinROI + firstGazeWithinRegion([test.eyetracking(1).gaze(firstGazeIndexWithinROI + 1:end-1).left], screenRelativeRegion(screen, video, roi));
+    if firstGazeIndexWithinROI == length(test.eyetracking(1).gaze)
         break
     else
-        tail = head + 1;
-        lastTestedTail = head;
-        while regionContains(roi, videoRelativePoint(screen, video, test.eyetracking(1).gaze(tail).left))
-            lastTestedTail = tail;
-            if tail == length(test.eyetracking(1).gaze)
+        lastGazeIndexOutsideROI = firstGazeIndexWithinROI + 1;
+        while regionContains(roi, videoRelativePoint(screen, video, test.eyetracking(1).gaze(lastGazeIndexOutsideROI).left))
+            lastGazeIndexOutsideROI = lastGazeIndexOutsideROI + 1;
+            if lastGazeIndexOutsideROI == length(test.eyetracking(1).gaze) + 1
                 break
             end
-            tail = tail + 1;
         end
-        if lastTestedTail ~= head && gazeDuration_us(test.eyetracking(1).gaze(head:lastTestedTail)) >= 90000
-            fixations(end+1).duration_ms = gazeDuration_ms(test.eyetracking(1).gaze(head:lastTestedTail));
-            fixations(end).targetStartRelativeTime_ms = targetStartRelativeTime_ms(test.eyetracking(1), test.eyetracking(1).gaze(head).time_us);
+        if lastGazeIndexOutsideROI ~= firstGazeIndexWithinROI + 1 && gazeDuration_us(test.eyetracking(1).gaze(firstGazeIndexWithinROI:lastGazeIndexOutsideROI-1)) >= fixation.threshold.us
+            fixations(end+1).duration_ms = gazeDuration_ms(test.eyetracking(1).gaze(firstGazeIndexWithinROI:lastGazeIndexOutsideROI-1));
+            fixations(end).targetStartRelativeTime_ms = targetStartRelativeTime_ms(test.eyetracking(1), test.eyetracking(1).gaze(firstGazeIndexWithinROI).time_us);
         end
-        if tail == length(test.eyetracking(1).gaze) || tail == length(test.eyetracking(1).gaze) - 1
+        if lastGazeIndexOutsideROI > length(test.eyetracking(1).gaze) - 2
             break
         else
-            head = tail + 1;
+            firstGazeIndexWithinROI = lastGazeIndexOutsideROI;
         end
     end
 end
