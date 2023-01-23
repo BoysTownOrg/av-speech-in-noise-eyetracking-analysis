be364b53828d60ad12666b8ee80a93676fe560fa
Seth Bashford <seth.bashford@boystown.org>
2023-01-23 15:55:57 -0600

    refactor

--- +aspl/calculateFixations.m
+++ +aspl/calculateFixations.m
@@ -1,30 +1,37 @@
-function output = calculateFixations(results, roiMap, video)
+function trial = calculateFixations(results, roiMap, video)
 screen.pixels.height = 1080;
 screen.pixels.width = 1920;
 fixation.threshold.us = 90000;
 
-output(numel(results.trial)).fixations = struct([]);
-for trial = 1:numel(results.trial)
-    roi = roiMap(aspl.convertToRoiMapKey(results.trial(trial).target.char));
-    lastGazeIndexOutsideROI = 0;
-    while lastGazeIndexOutsideROI < gazeSamples(results, trial) - 1
-        firstGazeIndexWithinROI = lastGazeIndexOutsideROI + ...
-            aspl.firstGazeWithinRegion(...
-            [gazeBetween(results, trial, lastGazeIndexOutsideROI+1, gazeSamples(results, trial)-1).left], ...
-            aspl.screenRelativeRegion(screen, video, roi));
-        lastGazeIndexOutsideROI = firstGazeIndexWithinROI + 1;
-        while lastGazeIndexOutsideROI ~= gazeSamples(results, trial) + 1 && aspl.regionContains(roi, ...
-                aspl.videoRelativePoint(screen, video, results.trial(trial).eyetracking.gaze(lastGazeIndexOutsideROI).left))
-            lastGazeIndexOutsideROI = lastGazeIndexOutsideROI + 1;
-        end
-        lastGazeIndexWithinROI = lastGazeIndexOutsideROI - 1;
-        if lastGazeIndexWithinROI ~= firstGazeIndexWithinROI && ...
-                aspl.gazeDuration_us(gazeBetween(results, trial, firstGazeIndexWithinROI, lastGazeIndexWithinROI)) >= fixation.threshold.us
-            output(trial).fixations(end+1).duration_ms = aspl.gazeDuration_ms(...
-                gazeBetween(results, trial, firstGazeIndexWithinROI, lastGazeIndexWithinROI));
-            output(trial).fixations(end).targetStartRelativeTime_ms = aspl.targetStartRelativeTime_ms(...
-                results.trial(trial).eyetracking, results.trial(trial).eyetracking.gaze(firstGazeIndexWithinROI).time_us);
-        end
+trial(numel(results.trial)).fixations.left = struct([]);
+trial(numel(results.trial)).fixations.right = struct([]);
+for i = 1:numel(results.trial)
+    trial(i).fixations.left = tbd(results, roiMap, video, screen, fixation, i, @(gaze)gaze.left);
+    trial(i).fixations.right = tbd(results, roiMap, video, screen, fixation, i, @(gaze)gaze.right);
+end
+end
+
+function fixations = tbd(results, roiMap, video, screen, fixation, i, pointFromGaze)
+fixations = struct([]);
+roi = roiMap(aspl.convertToRoiMapKey(results.trial(i).target.char));
+lastGazeIndexOutsideROI = 0;
+while lastGazeIndexOutsideROI < gazeSamples(results, i) - 1
+    firstGazeIndexWithinROI = lastGazeIndexOutsideROI + ...
+        aspl.firstGazeWithinRegion(...
+        [pointFromGaze(gazeBetween(results, i, lastGazeIndexOutsideROI+1, gazeSamples(results, i)-1))], ...
+        aspl.screenRelativeRegion(screen, video, roi));
+    lastGazeIndexOutsideROI = firstGazeIndexWithinROI + 1;
+    while lastGazeIndexOutsideROI ~= gazeSamples(results, i) + 1 && aspl.regionContains(roi, ...
+            aspl.videoRelativePoint(screen, video, pointFromGaze(results.trial(i).eyetracking.gaze(lastGazeIndexOutsideROI))))
+        lastGazeIndexOutsideROI = lastGazeIndexOutsideROI + 1;
+    end
+    lastGazeIndexWithinROI = lastGazeIndexOutsideROI - 1;
+    if lastGazeIndexWithinROI ~= firstGazeIndexWithinROI && ...
+            aspl.gazeDuration_us(gazeBetween(results, i, firstGazeIndexWithinROI, lastGazeIndexWithinROI)) >= fixation.threshold.us
+        fixations(end+1).duration_ms = aspl.gazeDuration_ms(...
+            gazeBetween(results, i, firstGazeIndexWithinROI, lastGazeIndexWithinROI));
+        fixations(end).targetStartRelativeTime_ms = aspl.targetStartRelativeTime_ms(...
+            results.trial(i).eyetracking, results.trial(i).eyetracking.gaze(firstGazeIndexWithinROI).time_us);
     end
 end
 end
--- +aspl/parseAvSpeechEyetrackingOutput.m
+++ +aspl/parseAvSpeechEyetrackingOutput.m
@@ -1,64 +1,64 @@
-function test = parseAvSpeechEyetrackingOutput(file)
+function output = parseAvSpeechEyetrackingOutput(file)
 nextLine = file.nextLine();
 while ~isempty(nextLine)
     labelWithEntry = splitCharArray(nextLine, ': ');
     switch labelWithEntry(1)
         case "tester"
-            test.tester = labelWithEntry(2);
+            output.tester = labelWithEntry(2);
         case "subject"
-            test.subject = labelWithEntry(2);
+            output.subject = labelWithEntry(2);
         case "session"
-            test.session = labelWithEntry(2);
+            output.session = labelWithEntry(2);
         case "method"
-            test.method = labelWithEntry(2);
+            output.method = labelWithEntry(2);
         case "RME setting"
-            test.rmeSetting = labelWithEntry(2);
+            output.rmeSetting = labelWithEntry(2);
         case "transducer"
-            test.transducer = labelWithEntry(2);
+            output.transducer = labelWithEntry(2);
         case "masker"
-            test.masker = labelWithEntry(2);
+            output.masker = labelWithEntry(2);
         case "targets"
-            test.targets = labelWithEntry(2);
+            output.targets = labelWithEntry(2);
         case "masker level (dB SPL)"
-            test.maskerLevel_dB_SPL = integer(labelWithEntry(2));
+            output.maskerLevel_dB_SPL = integer(labelWithEntry(2));
         case "starting SNR (dB)"
-            test.starting_SNR_dB = integer(labelWithEntry(2));
+            output.starting_SNR_dB = integer(labelWithEntry(2));
         case "condition"
-            test.condition = labelWithEntry(2);
+            output.condition = labelWithEntry(2);
         case "up"
-            test.adaptive.up = integer(labelWithEntry(2));
+            output.adaptive.up = integer(labelWithEntry(2));
         case "down"
-            test.adaptive.down = integer(labelWithEntry(2));
+            output.adaptive.down = integer(labelWithEntry(2));
         case "reversals per step size"
-            test.adaptive.reversalsPerStepSize = integer(labelWithEntry(2));
+            output.adaptive.reversalsPerStepSize = integer(labelWithEntry(2));
         case "step sizes (dB)"
-            test.adaptive.stepSizes_dB = integer(labelWithEntry(2));
+            output.adaptive.stepSizes_dB = integer(labelWithEntry(2));
         case "threshold reversals"
-            test.adaptive.thresholdReversals = integer(labelWithEntry(2));
+            output.adaptive.thresholdReversals = integer(labelWithEntry(2));
     end
     nextLine = file.nextLine();
 end
-test.trial = struct([]);
+output.trial = struct([]);
 file.nextLine();
 nextLine = file.nextLine();
 while ischar(nextLine)
     entries = splitCharArray(nextLine, ', ');
-    test.trial(end+1).target = entries(2);
+    output.trial(end+1).target = entries(2);
     nextLine = file.nextLine();
     labelWithEntry = splitCharArray(nextLine, ': ');
-    test.trial(end).eyetracking.targetStartTime_ns = bigInteger(labelWithEntry(2));
+    output.trial(end).eyetracking.targetStartTime_ns = bigInteger(labelWithEntry(2));
     file.nextLine();
     entries = splitNextFileLine(file, ', ');
-    test.trial(end).eyetracking.syncTime.eyeTracker_us = bigInteger(entries(1));
-    test.trial(end).eyetracking.syncTime.targetPlayer_ns = bigInteger(entries(2));
+    output.trial(end).eyetracking.syncTime.eyeTracker_us = bigInteger(entries(1));
+    output.trial(end).eyetracking.syncTime.targetPlayer_ns = bigInteger(entries(2));
     file.nextLine();
-    test.trial(end).eyetracking.gaze = struct([]);
+    output.trial(end).eyetracking.gaze = struct([]);
     nextLine = file.nextLine();
     entries = splitCharArray(nextLine, ", ");
     while numel(entries) == 7 && numel(float(entries(2))) == 2
-        test.trial(end).eyetracking.gaze(end+1).time_us = bigInteger(entries(1));
-        test.trial(end).eyetracking.gaze(end).left = parseFloatPoint(entries(2));
-        test.trial(end).eyetracking.gaze(end).right = parseFloatPoint(entries(3));
+        output.trial(end).eyetracking.gaze(end+1).time_us = bigInteger(entries(1));
+        output.trial(end).eyetracking.gaze(end).left = parseFloatPoint(entries(2));
+        output.trial(end).eyetracking.gaze(end).right = parseFloatPoint(entries(3));
         nextLine = file.nextLine();
         entries = splitCharArray(nextLine, ", ");
     end
