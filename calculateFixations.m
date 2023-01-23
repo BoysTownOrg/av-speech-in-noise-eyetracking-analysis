8ba6d6578da3fd95bb6bc0211f24f4bb7ad769c3
Seth Bashford <seth.bashford@boystown.org>
2023-01-23 12:16:50 -0600

    more ideas

--- calculateFixations.m
+++ calculateFixations.m
@@ -1,35 +1,36 @@
-function fixations = calculateFixations(results)
+function output = calculateFixations(results, roiMap)
 screen.pixels.height = 1080;
 screen.pixels.width = 1920;
 video.pixels.height = 1080;
 video.pixels.width = 1920;
 video.scaling = 2/3;
 fixation.threshold.us = 90000;
-trial = 1;
 roi.x = 0.25;
 roi.y = 0.25;
 roi.width = 0.5;
 roi.height = 0.5;
 
-fixations = [];
-lastGazeIndexOutsideROI = 0;
-while lastGazeIndexOutsideROI < gazeSamples(results, trial) - 1
-    firstGazeIndexWithinROI = lastGazeIndexOutsideROI + ...
-        firstGazeWithinRegion(...
+output(numel(results.trial)).fixations = struct([]);
+for trial = 1:numel(results.trial)
+    lastGazeIndexOutsideROI = 0;
+    while lastGazeIndexOutsideROI < gazeSamples(results, trial) - 1
+        firstGazeIndexWithinROI = lastGazeIndexOutsideROI + ...
+            firstGazeWithinRegion(...
             [gazeBetween(results, trial, lastGazeIndexOutsideROI+1, gazeSamples(results, trial)-1).left], ...
             screenRelativeRegion(screen, video, roi));
-    lastGazeIndexOutsideROI = firstGazeIndexWithinROI + 1;
-    while lastGazeIndexOutsideROI ~= gazeSamples(results, trial) + 1 && regionContains(roi, ...
-            videoRelativePoint(screen, video, results.trial(trial).eyetracking.gaze(lastGazeIndexOutsideROI).left))
-        lastGazeIndexOutsideROI = lastGazeIndexOutsideROI + 1;
-    end
-    lastGazeIndexWithinROI = lastGazeIndexOutsideROI - 1;
-    if lastGazeIndexWithinROI ~= firstGazeIndexWithinROI && ...
-            gazeDuration_us(gazeBetween(results, trial, firstGazeIndexWithinROI, lastGazeIndexWithinROI)) >= fixation.threshold.us
-        fixations(end+1).duration_ms = gazeDuration_ms(...
-            gazeBetween(results, trial, firstGazeIndexWithinROI, lastGazeIndexWithinROI));
-        fixations(end).targetStartRelativeTime_ms = targetStartRelativeTime_ms(...
-            results.trial(trial).eyetracking, results.trial(trial).eyetracking.gaze(firstGazeIndexWithinROI).time_us);
+        lastGazeIndexOutsideROI = firstGazeIndexWithinROI + 1;
+        while lastGazeIndexOutsideROI ~= gazeSamples(results, trial) + 1 && regionContains(roi, ...
+                videoRelativePoint(screen, video, results.trial(trial).eyetracking.gaze(lastGazeIndexOutsideROI).left))
+            lastGazeIndexOutsideROI = lastGazeIndexOutsideROI + 1;
+        end
+        lastGazeIndexWithinROI = lastGazeIndexOutsideROI - 1;
+        if lastGazeIndexWithinROI ~= firstGazeIndexWithinROI && ...
+                gazeDuration_us(gazeBetween(results, trial, firstGazeIndexWithinROI, lastGazeIndexWithinROI)) >= fixation.threshold.us
+            output(trial).fixations(end+1).duration_ms = gazeDuration_ms(...
+                gazeBetween(results, trial, firstGazeIndexWithinROI, lastGazeIndexWithinROI));
+            output(trial).fixations(end).targetStartRelativeTime_ms = targetStartRelativeTime_ms(...
+                results.trial(trial).eyetracking, results.trial(trial).eyetracking.gaze(firstGazeIndexWithinROI).time_us);
+        end
     end
 end
 end
--- example.m
+++ example.m
@@ -1,4 +1,5 @@
 function fixations = example
 filepath = "reference\Subject_DELETE_Session_testing_Experimenter_GD_2023-1-19-16-6-18.txt";
-fixations = calculateFixations(parseAvSpeechEyetrackingOutput(File(filepath)));
+load('reference\markersAndROIs.mat', 'T3data');
+fixations = calculateFixations(parseAvSpeechEyetrackingOutput(File(filepath)), convertToRoiMap(T3data));
 end
