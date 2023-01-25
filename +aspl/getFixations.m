7310a80295adb30bbeee744af91209f4da1ceaf3
Seth Bashford <seth.bashford@boystown.org>
2023-01-25 09:44:18 -0600

    free test

--- +aspl/getFixations.m
+++ +aspl/getFixations.m
@@ -1,16 +1,16 @@
 function fixations = getFixations(gaze, screenRelativeRoi, threshold_us)
 fixations.left = struct([]);
-lastGazeIndexOutsideROI = 0;
-while lastGazeIndexOutsideROI < numel(gaze) - 1
-    firstGazeIndexWithinROI = lastGazeIndexOutsideROI + ...
+firstGazeIndexOutsideROI = 0;
+while firstGazeIndexOutsideROI < numel(gaze) - 1
+    firstGazeIndexWithinROI = firstGazeIndexOutsideROI + ...
         aspl.firstGazeWithinRegion(...
-        [gaze(lastGazeIndexOutsideROI+1:end-1).left], ...
+        [gaze(firstGazeIndexOutsideROI+1:end-1).left], ...
         screenRelativeRoi);
-    lastGazeIndexOutsideROI = firstGazeIndexWithinROI + 1;
-    while lastGazeIndexOutsideROI ~= numel(gaze) + 1 && aspl.regionContains(screenRelativeRoi, gaze(lastGazeIndexOutsideROI).left)
-        lastGazeIndexOutsideROI = lastGazeIndexOutsideROI + 1;
+    firstGazeIndexOutsideROI = firstGazeIndexWithinROI + 1;
+    while firstGazeIndexOutsideROI ~= numel(gaze) + 1 && aspl.regionContains(screenRelativeRoi, gaze(firstGazeIndexOutsideROI).left)
+        firstGazeIndexOutsideROI = firstGazeIndexOutsideROI + 1;
     end
-    lastGazeIndexWithinROI = lastGazeIndexOutsideROI - 1;
+    lastGazeIndexWithinROI = firstGazeIndexOutsideROI - 1;
     if lastGazeIndexWithinROI ~= firstGazeIndexWithinROI && ...
             aspl.gazeDuration_us(gaze(firstGazeIndexWithinROI:lastGazeIndexWithinROI)) >= threshold_us
         fixations.left(end+1).duration_ms = aspl.gazeDuration_ms(...
